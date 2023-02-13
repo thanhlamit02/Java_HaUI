2f3eeb9727179ba1b0383c3d0d8d8e6dc6943072
Nguyễn Thành Lâm <laotuong2002@gmail.com>
2023-02-13 23:41:22 +0700

    Java at HaUI

---
+++ JavaApplication1/src/TestJava/JavaFirst.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TestJava;
+
+/**
+ *
+ * @author thanhlam02.haui
+ */
+public class JavaFirst {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        int a = 6;
+        int b = 8;
+        System.out.print("a + b = "+Integer.sum(a, b));
+    }
+}
+
+        
